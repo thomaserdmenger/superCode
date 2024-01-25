c907581e08efed5bebf5ccffe9f8632008769435
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-25 15:35:31 +0100

    Add JS Output Task_1_4

---
+++ 240125_JS_Intro/Output_1_4/assets/js/app.js
@@ -0,0 +1,15 @@
+// window.alert('Hello World');
+// window.prompt("What's your name?");
+// window.prompt("What's your name?", 'Cancel');
+// window.confirm('Do you agree with my opinion?');
+
+// let age = window.prompt("What's your age?");
+// console.log(age);
+
+let b = 5;
+let a = b * 5 - 3;
+// window.alert(a);
+
+let man = 'Marzio is the greatest man in the world:';
+let size = '2.72m';
+// window.confirm(`${man} ${size}`);
