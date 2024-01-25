6c6bf9c983607a73ed22e86a95df0fa6f0f93531
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-25 14:58:21 +0100

    Add JS Intro Task_1_12

---
+++ 240125_JS_Intro/Intro_1_12/assets/js/app.js
@@ -0,0 +1,23 @@
+let score = 5 + 5 * 10;
+console.log(score);
+
+score = (5 + 5) * 10;
+console.log(score);
+
+score = 0;
+score = score + 10;
+console.log(score);
+
+score += 10;
+console.log(score);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+console.log(zahl);
+
+zahl++;
+console.log(zahl);
+
+zahl--;
+console.log(zahl);
