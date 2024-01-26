5ba765111350814fdd189b7fea652538cd9a75a0
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-26 15:24:14 +0100

    Add JS Functions Task 1_3

---
+++ 240126_JS_Intro_2/Functions_1_3/assets/js/app.js
@@ -0,0 +1,7 @@
+function intro3(name, stadt, alter) {
+  console.log(
+    `Hallo, mein Name ist ${name}. ich bin ${alter} Jahre alt. Ich komme aus ${stadt}.`
+  );
+}
+
+intro3('SuperCoder', 'Düsseldorf', '27');
