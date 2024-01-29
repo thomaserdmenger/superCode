fff526cdf1e8048772a0376f656008aaf8b07dc4
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-29 16:47:40 +0100

    Refactor JS ClassList_2_1

--- 240129_JS_Values_Classes/JS_ClassList_2_1/assets/js/app.js
+++ 240129_JS_Values_Classes/JS_ClassList_2_1/assets/js/app.js
@@ -4,6 +4,13 @@ const heading = document.querySelector('h1');
 const changeStyle = (e) => {
   const element = e.target;
 
+  if (
+    !element.classList.contains('btn-style') &&
+    !element.classList.contains('btn-reset')
+  ) {
+    return;
+  }
+
   if (element.classList.contains('btn-style')) {
     heading.classList.add('heading');
   } else if (element.classList.contains('btn-reset')) {
