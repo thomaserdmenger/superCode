9e1233a263ffce62cd8b0bfe7a8d751bad133d80
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-31 14:17:10 +0100

    Add JS Conditionals_2_4

---
+++ 240131_JS_Conditionals/Conditionals_2_4/assets/js/app.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+  const equal = 5;
+
+  if (typeof a === 'number' && typeof b === 'number') {
+    if (a !== b) {
+      console.log(a + b);
+    } else {
+      console.log(equal);
+    }
+  } else {
+    console.log(`Your input is not a number`);
+  }
+};
+
+add(6, 4);
+add(2, 2);
+add('Steffen', 'Lisa');
