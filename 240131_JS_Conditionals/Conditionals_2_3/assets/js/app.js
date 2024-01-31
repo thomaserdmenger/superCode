43be7bbc424e1b0480401da000b29db46d21e378
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-01-31 14:11:03 +0100

    Add JS Conditionals_2_3

---
+++ 240131_JS_Conditionals/Conditionals_2_3/assets/js/app.js
@@ -0,0 +1,16 @@
+const compareNumbers = (input) => {
+  const number = 27;
+  const difference = input - number;
+  const multiplicator = 2;
+
+  if (input > 27) {
+    console.log(difference * multiplicator);
+  }
+};
+
+compareNumbers(27);
+compareNumbers(28);
+compareNumbers(37);
+compareNumbers(35);
+compareNumbers(74);
+compareNumbers(123);
