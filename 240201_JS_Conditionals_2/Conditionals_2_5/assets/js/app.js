a8d27e5533cd079898e7b2e4395210efb2c1909b
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-01 15:15:52 +0100

    Refactor JS Conditionals_2_5

--- 240201_JS_Conditionals_2/Conditionals_2_5/assets/js/app.js
+++ 240201_JS_Conditionals_2/Conditionals_2_5/assets/js/app.js
@@ -2,71 +2,71 @@ const btn = document.querySelector('input[type="button"]');
 
 const check = () => {
   const userInput = document.querySelector('input[type="text"]');
-  let userInputVal = userInput.value;
+  let userInputVal = userInput.value.toLowerCase();
   const message = document.querySelector('#bundeslandInfoErgebnis');
 
   switch (userInputVal) {
-    case 'Baden-Württemberg':
+    case 'baden-württemberg':
       message.textContent =
         'Baden-Württemberg hat 10,880 Mio Einwohner und Stuttgart ist die Hauptstadt';
       break;
-    case 'Bayern':
+    case 'bayern':
       message.textContent =
         'Bayern hat 12,844 Mio Einwohner und München ist die Hauptstadt';
       break;
-    case 'Berlin':
+    case 'berlin':
       message.textContent =
         'Berlin hat 3,520 Mio Einwohner und Berlin ist die Hauptstadt';
       break;
-    case 'Brandenburg':
+    case 'brandenburg':
       message.textContent =
         'Brandenburg hat 2,485 Mio Einwohner und Potsdam ist die Hauptstadt';
       break;
-    case 'Bremen':
+    case 'bremen':
       message.textContent =
         'Bremen hat 0,671 Mio Einwohner und Bremen ist die Hauptstadt';
       break;
-    case 'Hamburg':
+    case 'hamburg':
       message.textContent =
         'Hamburg hat 1,787 Mio Einwohner und Hamburg ist die Hauptstadt';
       break;
-    case 'Hessen':
+    case 'hessen':
       message.textContent =
         'Hessen hat 6,176 Mio Einwohner und Wiesbaden ist die Hauptstadt';
       break;
-    case 'Mecklenburg-Vorpommern':
+    case 'mecklenburg-vorpommern':
       message.textContent =
         'Mecklenburg-Vorpommern hat 1,612 Mio Einwohner und Schwerin ist die Hauptstadt';
       break;
-    case 'Niedersachsen':
+    case 'niedersachsen':
       message.textContent =
         'Niedersachsen hat 7,927 Mio Einwohner und Hannover ist die Hauptstadt';
       break;
-    case 'Nordrhein-Westfalen':
+    case 'nordrhein-westfalen':
       message.textContent =
         'Nordrhein-Westfalen hat 17,865 Mio Einwohner und Düsseldorf ist die Hauptstadt';
       break;
-    case 'Rheinland-Pfalz':
+    case 'rheinland-pfalz':
       message.textContent =
         'Rheinland-Pfalz hat 4,053 Mio Einwohner und Mainz ist die Hauptstadt';
       break;
-    case 'Saarland':
+    case 'saarland':
       message.textContent =
         'Saarland hat 0,996 Mio Einwohner und Saarbrücken ist die Hauptstadt';
       break;
-    case 'Sachsen':
+    case 'sachsen':
       message.textContent =
         'Sachsen hat 4,085 Mio Einwohner und Dresden ist die Hauptstadt';
       break;
-    case 'Sachsen-Anhalt':
+    case 'sachsen-anhalt':
       message.textContent =
         'Sachsen-Anhalt hat 2,245 Mio Einwohner und Magdeburg ist die Hauptstadt';
       break;
-    case 'Schleswig-Holstein':
+    case 'schleswig-holstein':
       message.textContent =
         'Schleswig-Holstein hat 2,859 Mio Einwohner und Kiel ist die Hauptstadt';
       break;
-    case 'Thüringen':
+    case 'thüringen':
       message.textContent =
         'Thüringen hat 2,171 Mio Einwohner und Erfurt ist die Hauptstadt';
       break;
