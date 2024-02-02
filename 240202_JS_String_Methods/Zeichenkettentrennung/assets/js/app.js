91c530f7bed60a566f435cd4d9e0228459974fa8
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-02 14:55:12 +0100

    Add Project Zeichenkettentrennungsmaschine

---
+++ 240202_JS_String_Methods/Zeichenkettentrennung/assets/js/app.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('form');
+
+const divideString = (event) => {
+  event.preventDefault();
+
+  const userStringVal = document
+    .querySelector('#user-input')
+    .value.toLowerCase();
+  const userDividerVal = document
+    .querySelector('#user-divider')
+    .value.toLowerCase();
+  const radioBtnOne = document.querySelector(
+    'input[type="radio"]:nth-of-type(1)'
+  );
+  const radioBtnTwo = document.querySelector(
+    'input[type="radio"]:nth-of-type(2)'
+  );
+  const resultPartOne = document.querySelector('.davor');
+  const resultPartTwo = document.querySelector('.danach');
+  const errorMessage = document.querySelector('.error');
+  const index = userStringVal.indexOf(userDividerVal);
+
+  if (index === -1) {
+    errorMessage.textContent = 'Bitte gib einen validen Trenner ein';
+    // errorMessage.style.color = 'red';
+    resultPartOne.textContent = '';
+    resultPartTwo.textContent = '';
+  } else if (radioBtnOne.checked) {
+    const partOne = userStringVal.slice(0, index);
+    const partTwo = userStringVal.slice(index, userStringVal.length);
+    resultPartOne.textContent = partOne;
+    resultPartTwo.textContent = partTwo;
+    errorMessage.textContent = '';
+  } else if (radioBtnTwo.checked) {
+    const partOne = userStringVal.slice(0, index + 1);
+    const partTwo = userStringVal.slice(index + 1, userStringVal.length);
+    resultPartOne.textContent = partOne;
+    resultPartTwo.textContent = partTwo;
+    errorMessage.textContent = '';
+  } else {
+    errorMessage.textContent = 'Etwas ist schiefgelaufen. Versuche es erneut.';
+  }
+};
+
+form.addEventListener('submit', divideString);
