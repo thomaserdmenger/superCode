f66600e1e50ab3acf111df1650060a61ba0beb07
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-02 13:30:50 +0100

    Add JS String_Methods_1_3

---
+++ 240202_JS_String_Methods/String_Methods_1_3/assets/js/app.js
@@ -0,0 +1,11 @@
+let txt2 =
+  'Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent.';
+
+const posSemicolon = txt2.search(';');
+console.log(posSemicolon);
+
+const posGreen = txt2.search('green');
+console.log(posGreen);
+
+const posBlue = txt2.search('blue');
+console.log(posBlue);
