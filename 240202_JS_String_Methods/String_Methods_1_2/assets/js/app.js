348465f6d3322b162a756d288588e178d69cde37
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-02 13:23:46 +0100

    Add JS String_Methods_1_2

---
+++ 240202_JS_String_Methods/String_Methods_1_2/assets/js/app.js
@@ -0,0 +1,11 @@
+const txt =
+  'How inappropriate to call this planet Earth, when clearly it is Ocean.';
+
+const posH = txt.indexOf('h');
+console.log(posH); // 27
+
+const posEarth = txt.indexOf('Earth');
+console.log(posEarth); // 38
+
+const posMoon = txt.indexOf('Moon');
+console.log(posMoon); // -1
