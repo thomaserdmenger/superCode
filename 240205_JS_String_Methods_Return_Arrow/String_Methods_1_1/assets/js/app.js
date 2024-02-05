75b878995ed36c494cd167f89ea6cee3d4dd721f
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-05 11:26:27 +0100

    Add JS String_Methods_1_1

---
+++ 240205_JS_String_Methods_Return_Arrow/String_Methods_1_1/assets/js/app.js
@@ -0,0 +1,7 @@
+const firstName = 'Sharon';
+const lastName = 'Van Etten';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(firstName.length);
+console.log(lastName.length);
+console.log(fullName.length);
