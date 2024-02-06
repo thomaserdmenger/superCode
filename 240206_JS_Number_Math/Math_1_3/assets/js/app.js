c05beafe6fde0a9d9243177956e30f041728754c
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-06 11:58:13 +0100

    Add JS Math Object Methods_1_3

---
+++ 240206_JS_Number_Math/Math_1_3/assets/js/app.js
@@ -0,0 +1,8 @@
+const randomNum = Math.random();
+console.log(randomNum);
+
+const randomNum1 = Math.ceil(Math.random() * 10);
+console.log(randomNum1);
+
+const randomNum2 = Math.ceil(Math.random() * 100);
+console.log(randomNum2);
