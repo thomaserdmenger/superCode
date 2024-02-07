86ad8cec0009169c0580d7a0a13db12d511b7862
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-07 13:04:08 +0100

    Add Array_1_2

---
+++ 240207_JS_Arrays/Arrays_1_2/assets/js/app.js
@@ -0,0 +1,7 @@
+const person = ['john doe', 'male', 23];
+const friends = ['marzio', 'lisa', 'steffen'];
+const favoriteFood = ['avocado', 'nuts', 'fruits'];
+
+person.map((person) => console.log(person));
+friends.map((friend) => console.log(friend));
+favoriteFood.map((food) => console.log(food));
