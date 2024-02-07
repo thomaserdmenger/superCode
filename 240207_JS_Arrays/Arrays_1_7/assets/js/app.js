750bcf88722a892d77de5b28a4ae19e0f3f95276
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-07 13:13:31 +0100

    Add Array_1_7

---
+++ 240207_JS_Arrays/Arrays_1_7/assets/js/app.js
@@ -0,0 +1,10 @@
+const friends = ['marzio', 'lisa', 'steffen'];
+const favoriteFood = ['avocado', 'nuts', 'fruits'];
+console.log(friends);
+console.log(favoriteFood);
+
+friends.unshift('lucas', 'dieter');
+console.log(friends);
+
+favoriteFood.unshift('cherry', 'melone');
+console.log(favoriteFood);
