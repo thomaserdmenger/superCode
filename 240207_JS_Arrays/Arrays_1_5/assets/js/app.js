637a72a1767a85ce76bf773e7f801b5a4beed435
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-07 13:10:41 +0100

    Add Array_1_5

---
+++ 240207_JS_Arrays/Arrays_1_5/assets/js/app.js
@@ -0,0 +1,6 @@
+const favoriteFood = ['avocado', 'nuts', 'fruits', 'popcorn'];
+console.log(favoriteFood);
+
+const popedFood = favoriteFood.pop();
+console.log(popedFood);
+console.log(favoriteFood);
