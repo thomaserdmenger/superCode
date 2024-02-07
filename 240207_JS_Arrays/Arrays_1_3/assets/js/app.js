5eda8537656c2c41af01b76308e2a13fa27f7880
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-07 13:05:44 +0100

    Add Array_1_3

---
+++ 240207_JS_Arrays/Arrays_1_3/assets/js/app.js
@@ -0,0 +1,7 @@
+const person = ['john doe', 'male', 23];
+const friends = ['marzio', 'lisa', 'steffen'];
+const favoriteFood = ['avocado', 'nuts', 'fruits'];
+
+console.log(person.length);
+console.log(friends.length);
+console.log(favoriteFood.length);
