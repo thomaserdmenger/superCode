212c71f9537b7f7dfc3bda193819834a8b3b54fc
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-08 15:09:59 +0100

    Refactor JS Map_1_4

--- 240208_JS_Arrays/Map_1_4/assets/js/app.js
+++ 240208_JS_Arrays/Map_1_4/assets/js/app.js
@@ -1,8 +1,6 @@
 let fahrenheit = [0, 32, 45, 50, 75, 80, 99, 120];
 
-const calculate = (arr) => {
-  return arr.map((num) => Math.round((num - 32) / 1.8));
-};
+const calculate = (arr) => arr.map((num) => Math.round((num - 32) / 1.8));
 
 const celsius = calculate(fahrenheit);
 console.log(celsius);
