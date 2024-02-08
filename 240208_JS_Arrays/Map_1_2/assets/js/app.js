81029ddaf28ba686341ed49236a0a66d8f18f611
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-08 14:09:53 +0100

    Add JS Arrow Map_1_2

---
+++ 240208_JS_Arrays/Map_1_2/assets/js/app.js
@@ -0,0 +1,14 @@
+const drinks = [
+  'Coca-Cola',
+  'Apfelsaft',
+  'Pepsi',
+  'Traubensaft',
+  'Sprite',
+  'Orangensaft',
+  'Red Bull Energy Drink',
+  'Fanta',
+];
+
+const result = drinks.map((drink) => drink.toUpperCase());
+
+console.log(result);
