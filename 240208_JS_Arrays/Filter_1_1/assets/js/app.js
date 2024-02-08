3a6ecbcfe79ad3165d63360cc99a3102b914c1b2
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-08 13:26:11 +0100

    Add JS Arrow Filter_1_1

---
+++ 240208_JS_Arrays/Filter_1_1/assets/js/app.js
@@ -0,0 +1,16 @@
+const words = [
+  'Banane',
+  'Kaktus',
+  'Flausch',
+  'Ente',
+  'Apfel',
+  'Auto',
+  'Giraffe',
+  'Schmetterling',
+  'Krokodil',
+  'Lampe',
+];
+
+const result = words.filter((word) => word.length <= 6);
+
+console.log(result);
