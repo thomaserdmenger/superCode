16a824f22c36c80d43cfaef65b2e219cda40ec8e
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-08 14:14:01 +0100

    Add JS ForEach Map_1_1

---
+++ 240208_JS_Arrays/ForEach_1_1/assets/js/app.js
@@ -0,0 +1,14 @@
+let getraenke = [
+  'Coca-Cola',
+  'Apfelsaft',
+  'Pepsi',
+  'Traubensaft',
+  'Sprite',
+  'Orangensaft',
+  'Red Bull Energy Drink',
+  'Fanta',
+];
+
+const myDrinks = (arr) => arr.forEach((drink) => console.log(drink));
+
+myDrinks(getraenke);
