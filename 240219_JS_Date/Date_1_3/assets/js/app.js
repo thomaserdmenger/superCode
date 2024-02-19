460fa92d17a9690c9d0d3c972c2b03a0d8c90c37
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-19 13:59:47 +0100

    Add Solution for Date_1_3

---
+++ 240219_JS_Date/Date_1_3/assets/js/app.js
@@ -0,0 +1,21 @@
+const date1 = new Date();
+date1.setFullYear(2222);
+date1.setMonth(5);
+date1.setDate(20);
+date1.setHours(15);
+date1.setMinutes(42);
+date1.setSeconds(38);
+
+console.log(date1);
+
+const date2 = new Date();
+date2.setFullYear(2123);
+date2.setMonth(1);
+date2.setDate(24);
+date2.setHours(15);
+date2.setMinutes(42);
+date2.setSeconds(38);
+
+console.log(date2);
+
+// And so on
