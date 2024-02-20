57466b237bf65fe66f68b23bc99755890d6f8337
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-20 12:14:42 +0100

    Add Solution BOM_1_3

---
+++ 240220_JS_BOM/JS_BOM_1_3/assets/js/app.js
@@ -0,0 +1,9 @@
+console.log(window.navigator.userAgent);
+console.log(window.navigator.platform);
+console.log(window.navigator.appVersion);
+console.log(window.screen.width);
+console.log(window.screen.height);
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(window.screen.colorDepth);
+console.log(window.screen.pixelDepth);
