d754fe9985c0f4f86913de70deea64e83f2db7b5
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-21 14:11:10 +0100

    Refactor Fetch Function

--- 240221_JS_Fetch/app.js
+++ 240221_JS_Fetch/app.js
@@ -4,18 +4,21 @@ const gridContainer = document.querySelector('.grid')
 // Fetch Data from API
 fetch('https://picsum.photos/v2/list')
   .then((res) => res.json())
-  .then((data) => {
-    const result = data
-      .map((item) => {
-        return `
-            <div>
-                <img src="${item.download_url}" alt="${item.author}" />
-                <p>${item.author}</p>
-                <a href="${item.url}" target="_blank">See more</a>
-            </div>
-        `
-      })
-      .join('')
+  .then((data) => renderContent(data))
 
-    gridContainer.innerHTML = result
-  })
+// Render Content
+const renderContent = (data) => {
+  const result = data
+    .map((item) => {
+      return `
+          <div>
+              <img src="${item.download_url}" alt="${item.author}" />
+              <p>${item.author}</p>
+              <a href="${item.url}" target="_blank">See more</a>
+          </div>
+      `
+    })
+    .join('')
+
+  gridContainer.innerHTML = result
+}
